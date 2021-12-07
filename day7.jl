e7dfe5c4056ef88293bc2ff1d1c7b76b465b9240
Joseph Chou <joe.nicu@gmail.com>
2021-12-07 10:26:08 -0500

    Day 7, fix edge case and add part choice to function

--- day7.jl
+++ day7.jl
@@ -1,27 +1,25 @@
-# file = "data/day7_test.txt" # part 1 = 37; part 2 = 168
-file = "data/day7.txt" # part 1 = 352331; part 2 = 99266250
-
-positions = parse.(Int, split(readline(file), ','))
-
-fuelcost = function(positions, x)
+fuelcost = function(positions, x, staticcost = true)
     dist = positions .- x .|> abs
-
-    return (dist |> sum) # part 1
-    # return ((dist .* (dist .+ 1 ) .÷ 2) |> sum) # part 2
-
+    if staticcost # Part 1
+        return (dist |> sum) # part 1
+    else # Part 2
+        return ((dist .* (dist .+ 1 ) .÷ 2) |> sum) # part 2
+    end
 end
 
-# minimum([fuelcost(positions, i) for i in minimum(positions):maximum(positions)])
+# if cost function might not have a SINGLE local minimum, test over entire dataset:
+#   minimum([fuelcost(positions, i) for i in minimum(positions):maximum(positions)])
 
 
-lowestcost = function(positions)
+lowestcost = function(file; staticcost = true)
+    positions = parse.(Int, split(readline(file), ','))
     low = minimum(positions)
     high = maximum(positions)
     while low != high
-        lowcost = fuelcost(positions, low)
-        highcost = fuelcost(positions, high)
+        lowcost = fuelcost(positions, low, staticcost)
+        highcost = fuelcost(positions, high, staticcost)
     
-        # fencepost failure: if low / high differ by only one, then return the lower cost of the two
+        # edge case failure: if low / high differ by only one, then return the lower cost of the two
         if high - low <= 1
             if lowcost <= highcost
                 return(low, lowcost)
@@ -35,11 +33,14 @@ lowestcost = function(positions)
             low = mid
         elseif highcost > lowcost
             high = mid
-        else
+        else # edge case failure: if symetrically equipotential on either side of minimum
             low += 1
         end
     end
     return(mid, fuelcost(positions, mid))
 end
 
-lowestcost(positions)
+# file = "data/day7_test.txt" # part 1 = 37; part 2 = 168
+file = "data/day7.txt" # part 1 = 352331; part 2 = 99266250
+
+lowestcost(file, staticcost = true)[2]
