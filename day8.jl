bfeffcb308e78e1df1f6aeca52ce6a36bf99c1dc
Joseph Chou <joe.nicu@gmail.com>
2021-12-08 02:48:11 -0500

    day 8 parse output

--- day8.jl
+++ day8.jl
@@ -15,77 +15,90 @@ end
 part1(file)
 
 
-function part2(file)
-    lines = readlines(file)
-    for line in lines
-        line = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
-        str_to_int = Dict{String, Int}()
-        int_to_str = Dict{Int64, String}()
-        m = match(r"(.*) \| (.*)", line)
-        # m === nothing && continue
-        signals = m.captures[1]
-        output = m.captures[2]
-        signal = split(signals)
-
-        # unique length segments: 1 -> 2; 4 -> 4; 7 -> 3; 8 -> 7
-        for (idx, val) in enumerate(signal)
-            if length(signal[idx]) == 2
-                str_to_int[signal[idx]] = 1
-                int_to_str[1] = signal[idx]
-            elseif length(signal[idx]) == 4
-                str_to_int[signal[idx]] = 4
-                int_to_str[4] = signal[idx]
-            elseif length(signal[idx]) == 3
-                str_to_int[signal[idx]] = 7
-                int_to_str[7] = signal[idx]
-            elseif length(signal[idx]) == 7
-                str_to_int[signal[idx]] = 8
-                int_to_str[8] = signal[idx]
-            end
-        end
-
-        # 5 segment that has '1' Set(int_to_str[1]) is a 3
-        for (idx, val) in enumerate(signal)
-            if length(val) == 5 && (Set(int_to_str[1]) ⊆ Set(signal[idx]))
-                str_to_int[signal[idx]] = 3
-                int_to_str[3] = signal[idx]
-            end
-        end
-
-        # 6 segment that does NOT have '1' is 6
-        for (idx, val) in enumerate(signal)
-            if length(val) == 6 && !(Set(int_to_str[1]) ⊆ Set(signal[idx]))
-                str_to_int[signal[idx]] = 6
-                int_to_str[6] = signal[idx]
-            end
-        end
-
-        # UNIDENTIFIED 6 segment that does NOT has 3 is 9, otherwise 0
-        for (idx, val) in enumerate(signal)
-            if length(val) == 6 && !haskey(str_to_int, val)
-                if Set(int_to_str[3]) ⊆ Set(signal[idx])
-                    str_to_int[signal[idx]] = 9
-                    int_to_str[9] = signal[idx]
-                else
-                    str_to_int[signal[idx]] = 0
-                    int_to_str[0] = signal[idx]
-                end
-            end
-        end
-
-        # UNIDENTIFIED 5 segment that is a subset of 6 is a 5, otherwise 2
-        for (idx, val) in enumerate(signal)
-            if length(val) == 5 && !haskey(str_to_int, val)
-                if Set(signal[idx]) ⊆ Set(int_to_str[6])
-                    str_to_int[signal[idx]] = 5
-                    int_to_str[5] = signal[idx]
-                else
-                    str_to_int[signal[idx]] = 2
-                    int_to_str[2] = signal[idx]
-                end
-            end
-        end
-        
-    end
+function parse_line(line)
+    str_to_int = Dict{String, Int}()
+    int_to_str = Dict{Int, String}()
+    m = match(r"(.*) \| (.*)", line)
+    # m === nothing && continue
+    signals = m.captures[1]
+    outputs = m.captures[2]
     
+    # signal = split(signals)
+    signal = signals |> split .|> collect .|> sort .|> join
+
+    # unique length segments: 1 -> 2; 4 -> 4; 7 -> 3; 8 -> 7
+    for (idx, val) in enumerate(signal)
+        if length(signal[idx]) == 2
+            str_to_int[signal[idx]] = 1
+            int_to_str[1] = signal[idx]
+        elseif length(signal[idx]) == 4
+            str_to_int[signal[idx]] = 4
+            int_to_str[4] = signal[idx]
+        elseif length(signal[idx]) == 3
+            str_to_int[signal[idx]] = 7
+            int_to_str[7] = signal[idx]
+        elseif length(signal[idx]) == 7
+            str_to_int[signal[idx]] = 8
+            int_to_str[8] = signal[idx]
+        end
+    end
+
+    # 5 segment that has '1' Set(int_to_str[1]) is a 3
+    for (idx, val) in enumerate(signal)
+        if length(val) == 5 && (Set(int_to_str[1]) ⊆ Set(signal[idx]))
+            str_to_int[signal[idx]] = 3
+            int_to_str[3] = signal[idx]
+        end
+    end
+
+    # 6 segment that does NOT have '1' is 6
+    for (idx, val) in enumerate(signal)
+        if length(val) == 6 && !(Set(int_to_str[1]) ⊆ Set(signal[idx]))
+            str_to_int[signal[idx]] = 6
+            int_to_str[6] = signal[idx]
+        end
+    end
+
+    # UNIDENTIFIED 6 segment that does NOT has 3 is 9, otherwise 0
+    for (idx, val) in enumerate(signal)
+        if length(val) == 6 && !haskey(str_to_int, val)
+            if Set(int_to_str[3]) ⊆ Set(signal[idx])
+                str_to_int[signal[idx]] = 9
+                int_to_str[9] = signal[idx]
+            else
+                str_to_int[signal[idx]] = 0
+                int_to_str[0] = signal[idx]
+            end
+        end
+    end
+
+    # UNIDENTIFIED 5 segment that is a subset of 6 is a 5, otherwise 2
+    for (idx, val) in enumerate(signal)
+        if length(val) == 5 && !haskey(str_to_int, val)
+            if Set(signal[idx]) ⊆ Set(int_to_str[6])
+                str_to_int[signal[idx]] = 5
+                int_to_str[5] = signal[idx]
+            else
+                str_to_int[signal[idx]] = 2
+                int_to_str[2] = signal[idx]
+            end
+        end
+    end
+
+    # Parse outputs
+    x = [string(str_to_int[output]) for output in (outputs |> split .|> collect .|> sort .|> join)]
+    
+    return(int_to_str, str_to_int, parse(Int, join(x)))
 end
+
+line = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+
+parse_line(line)
+
+# lines = readlines(file)
+# for line in lines
+    
+# end
+
+x = [string(str_to_int[output]) for output in (outputs |> split .|> collect .|> sort .|> join)]
+parse(Int, join(x))
