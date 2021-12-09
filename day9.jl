f7715f5d0595e91576671d71f02d9976bab0887a
Joseph Chou <joe.nicu@gmail.com>
2021-12-09 15:45:42 -0500

    Day 9, more refactoring and comments

--- day9.jl
+++ day9.jl
@@ -1,12 +1,10 @@
-# file = "data/day9_test.txt" # part 1 = 15; part 2 = 1134
-file = "data/day9.txt" # part 1 = 456; part 2 = 1047744
-
 function day9(file)
+    # Convert file of grid of digits into matrix of Int
     # https://www.assertnotmagic.com/2019/05/17/julia-read-grid/
     lines = readlines(file) .|> collect .|> x -> parse.(Int, x)
     m = permutedims(hcat(lines...)) # without ..., get 5x1 matrix of Vector, instead of 10x5 matrix of Int
 
-    # create matrix pad_m which is 1 larger in each dimension, padded with 9
+    # create matrix pad_m which m padded with 9's; will be 2 larger in each dimension than m
     pad_m = fill(9, size(m) .+ 2)
     (x, y) = size(m)
     pad_m[2:(x+1), 2:(y+1)] = m # replace interior of n by m --> m surrounded by 9's
@@ -23,8 +21,7 @@ function day9(file)
     part1 = sum(risklevels)
 
     ##### Part 2 #####
-    function search_basin(x, y) # recursively determine basin size
-        # needs access to global 'track' matrix
+    function search_basin(x, y) # recursive function to determine basin size; needs access to global 'track' matrix
         if track[x, y] == '0' # blocked
             return 0
         end
@@ -38,19 +35,20 @@ function day9(file)
     end
     
     basin_m = pad_m .!= 9 # matrix of Booleans, padded size, where groups of true are basins; need to add 1 to indices
-    sizes = Vector{Int}() # vector of basin sizes
+    sizes = Vector{Int}() # vector to store basin sizes as we find them
     for lowpoint in findall(lowpoints)
-        (x0, y0) = (lowpoint[1] + 1, lowpoint[2] + 1) # start point of basin
         global track = fill('.', size(basin_m)) # . untried, 0 blocked, 1 included; global so recursive function can update
-        track[.!basin_m] .= '0' # fill in all obstacles
-        basinsize = search_basin(x0, y0)
-        push!(sizes, basinsize)
+        track[.!basin_m] .= '0' # fill in all obstacles (9's)
+        (x0, y0) = (lowpoint[1] + 1, lowpoint[2] + 1) # start point of basin; add 1 to indices to index padded basin_m
+        push!(sizes, search_basin(x0, y0))
      end
      part2 = prod(sort(sizes, rev = true)[1:3]) # product of 3 largest basin sizes
 
      return(part1, part2)
 end
 
+# file = "data/day9_test.txt" # part 1 = 15; part 2 = 1134
+file = "data/day9.txt" # part 1 = 456; part 2 = 1047744
 
 (part1, part2) = day9(file)
 println("Part 1: $part1")
