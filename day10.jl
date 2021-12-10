610b4b650d4b20ba2be3e594a89441a063c53973
Joseph Chou <joe.nicu@gmail.com>
2021-12-10 05:16:20 -0500

    Day 10, part 1 + 2

--- day10.jl
+++ day10.jl
@@ -1,68 +1,54 @@
-# file = "data/day10_test.txt" # part 1 = 26397; part 2 = 
-file = "data/day10.txt" # part 1 = 392421; part 2 = 
-
-# for line in eachline(file)
-    # parens = square = curly = pointy = 0;
-    # for (idx, c) in enumerate(line)
-    #     c == '(' && (parens += 1)
-    #     c == '[' && (square += 1)
-    #     c == '{' && (curly += 1)
-    #     c == '<' && (pointy += 1)
-
-    #     if c == ')'
-    #         parens -= 1
-    #         parens < 0 && (println(")"); break)
-    #     end
-    #     if c == ']'
-    #         square -= 1
-    #         square < 0 && (println("]"); break)
-    #     end
-    #     if c == '}'
-    #         curly -= 1
-    #         curly < 0 && (println("}"); break)
-    #     end
-    #     if c == '>'
-    #         pointy -= 1
-    #         pointy < 0 && (println(">"); break)
-    #     end
-    # end
-# end
-
 openbrackets  = Dict('(' => ')', '[' => ']', '{' => '}', '<' => '>', )
 closebrackets = Dict(')' => '(', ']' => '[', '}' => '{', '>' => '<')
-bracketscore = Dict(')' => 3, ']' => 57, '}' => 1197, '>' => 25137)
-
-line = "{([(<{}[<>[]}>{[]{[(<()>"
+bracketscore = Dict(')' => 3, ']' => 57, '}' => 1197, '>' => 25137) # for part 1 scoring
+completionscore = Dict(')' => 1, ']' => 2, '}' => 3, '>' => 4) # for part 2 scoring
 
 function parseline(line)
     # println(line)
     if length(line) == 0
-        # println("Valid")
-        return(0)
+        return(("Valid", 0))
     end
     for (idx, c) in enumerate(line)
         if c ∈ keys(closebrackets)
             if line[idx-1] != closebrackets[c]
-                # println("Mismatch: $c")
-                return(bracketscore[c])
+                return("Corrupt", bracketscore[c])
             else
                 subline = line[1:idx-2] * line[idx+1:end]
-                # println(subline)
                 return(parseline(subline))
             end
         end
     end
-    # println("Incomplete")
-    return(0)
+    return(line, 0) # incomplete
 end
 
-part1 = function(file)
-    score = 0
-    for (idx, line) in enumerate(eachline(file))
-        # println("\n$idx $line")
-        score += parseline(line)
+day10 = function(file)
+    score1 = 0 # score for part 1
+    score2 = Vector{Int}() # vector to store completion scores for part 2as we find them
+
+    for line in eachline(file)
+        # Part 1
+        result = parseline(line)
+        score1 += result[2]
+
+        # Part 2
+        if result[1] != "Valid" && result[1] != "Corrupt" # incomplete
+            completion = [openbrackets[c] for c in result[1]] |> join |> reverse # construct required completion string
+            total = 0
+            for c in completion
+                total *= 5
+                total += completionscore[c]
+            end
+            push!(score2, total)
+        end
     end
-    return(score)
+    return(score1, sort(score2)[length(score2)÷2 + 1]) # middle score for part 2
 end
 
-println("Part 1: $(part1(file))")
+# file = "data/day10_test.txt" # part 1 = 26397; part 2 = 288957
+file = "data/day10.txt" # part 1 = 392421; part 2 = 2769449099
+
+results = day10(file)
+println("Part 1: $(results[1])")
+println("Part 2: $(results[2])")
+
+
