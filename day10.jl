65cdf1e519e3979c50bcc8a62d3150b6d94655de
Joseph Chou <joe.nicu@gmail.com>
2021-12-10 05:20:19 -0500

    Add explanatory comments

--- day10.jl
+++ day10.jl
@@ -4,7 +4,8 @@ bracketscore = Dict(')' => 3, ']' => 57, '}' => 1197, '>' => 25137) # for part 1
 completionscore = Dict(')' => 1, ']' => 2, '}' => 3, '>' => 4) # for part 2 scoring
 
 function parseline(line)
-    # println(line)
+    # Recursively remove consecutive matching open + close bracketscore
+    # Returns tuple of ("Valid" OR "Corrupt" OR remaining unmatched brackets) and (part 1 score)
     if length(line) == 0
         return(("Valid", 0))
     end
@@ -13,12 +14,12 @@ function parseline(line)
             if line[idx-1] != closebrackets[c]
                 return("Corrupt", bracketscore[c])
             else
-                subline = line[1:idx-2] * line[idx+1:end]
-                return(parseline(subline))
+                subline = line[1:idx-2] * line[idx+1:end] # remove matching brackets
+                return(parseline(subline)) # recursively apply to remaining
             end
         end
     end
-    return(line, 0) # incomplete
+    return(line, 0) # incomplete, so return remaining brackets that need to be matched
 end
 
 day10 = function(file)
@@ -44,11 +45,9 @@ day10 = function(file)
     return(score1, sort(score2)[length(score2)÷2 + 1]) # middle score for part 2
 end
 
-# file = "data/day10_test.txt" # part 1 = 26397; part 2 = 288957
-file = "data/day10.txt" # part 1 = 392421; part 2 = 2769449099
+file = "data/day10_test.txt" # part 1 = 26397; part 2 = 288957
+# file = "data/day10.txt" # part 1 = 392421; part 2 = 2769449099
 
 results = day10(file)
 println("Part 1: $(results[1])")
 println("Part 2: $(results[2])")
-
-
