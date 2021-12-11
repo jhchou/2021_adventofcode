bd14e034cae3dd4ee122d48877e38d7417244c1d
Joseph Chou <joe.nicu@gmail.com>
2021-12-11 05:18:47 -0500

    day 11, part 1+2, but code in giant blob

--- day11.jl
+++ day11.jl
@@ -1,13 +1,37 @@
-
-
-part1 = function(file)
+takestep! = function(pad_m)
+    numberflashed = 0
+    pad_m .+= 1 # increase energy by 1
+    flashed = Set{CartesianIndex}() # what has flashed this step
+    while true
+        anyflashed = false
+        toflash = findall(pad_m .> 9) # all coordinates > 9
+        for crd in toflash
+            if !(crd in flashed) # already flashed
+                # Increment all adjacent by 1
+                x0 = crd[1]
+                y0 = crd[2]
+                for (x,y) in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+                    pad_m[x0 + x, y0 + y] += 1
+                end
+                anyflashed = true
+                numberflashed += 1
+                push!(flashed, crd)
+            end
+        end
+        if !anyflashed
+            break
+        end
+    end
+    for crd in flashed
+        pad_m[crd] = 0
+    end
+    return(numberflashed)
 end
 
 
-
-file = "data/day11_test.txt" # part 1 = ; part 2 = 
+# file = "data/day11_test.txt" # part 1 = ; part 2 = 
 # file = "data/day11_test2.txt" # part 1 = ; part 2 = 
-# file = "data/day11.txt" # part 1 = ; part 2 = 
+file = "data/day11.txt" # part 1 = ; part 2 = 
 
 # results = day10(file)
 # println("Part 1: $(results[1])")
@@ -32,33 +56,15 @@ pad_m = fill(-1000, size(m) .+ 2) # or typemin(Int)
 (x, y) = size(m)
 pad_m[2:(x+1), 2:(y+1)] = m # replace interior of n by m --> m surrounded by -Inf
 
-totalflashed = 0
+# Part 1
+# totalflashed = 0
+# for step in 1:100
+#     totalflashed += takestep!(pad_m)
+# end
+# totalflashed
 
-pad_m .+= 1 # increase energy by 1
-flashed = Set{CartesianIndex}() # what has flashed this step
-while true
-    anyflashed = false
-    toflash = findall(pad_m .> 9) # all coordinates > 9
-    for crd in toflash
-        if !(crd in flashed) # already flashed
-            # Increment all adjacent by 1
-            x0 = crd[1]
-            y0 = crd[2]
-            println("Flashing $(x0-1) $(y0-1)")
-            for (x,y) in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-                pad_m[x0 + x, y0 + y] += 1
-            end
-            anyflashed = true
-            totalflashed += 1
-            push!(flashed, crd)
-        end
-    end
-    if !anyflashed
-        break
-    end
+step = 1
+while takestep!(pad_m) != length(m)
+    step += 1
 end
-for crd in flashed
-    pad_m[crd] = 0
-end
-v()
-totalflashed
+step
