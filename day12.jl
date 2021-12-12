2dd4a3c25e432407d28633876b8bffadaedc58b2
Joseph Chou <joe.nicu@gmail.com>
2021-12-12 07:05:40 -0500

    Day 12, part 1 + 2; not completely cleaned up

--- day12.jl
+++ day12.jl
@@ -5,43 +5,58 @@ readnodes = function(file)
         m = match(r"(.*)-(.*)", line)
         m === nothing && continue
         (n1, n2) = m.captures
-        haskey(nodes, n1) ? push!(nodes[n1], n2) : nodes[n1] = [n2]
-        haskey(nodes, n2) ? push!(nodes[n2], n1) : nodes[n2] = [n1]
+        # can never revisit start or leave end
+        if n1 != "end" && n2 != "start"
+            haskey(nodes, n1) ? push!(nodes[n1], n2) : nodes[n1] = [n2] # n1 --> n2
+        end
+        if n2 != "end" && n1 != "start"
+            haskey(nodes, n2) ? push!(nodes[n2], n1) : nodes[n2] = [n1] # n2 --> n1
+        end
     end
     smallnodes = [node for node in keys(nodes) if all(char -> char in 'a':'z', collect(node))]
     return (nodes, smallnodes)
 end
 
 
-
-(nodes, smallnodes) = readnodes(file)
-
-pathsearch = function(newnode::String, visited::Array{String})
+pathsearch = function(newnode::String, visited::Array{String}, maxvisits = 1) # number of times small nodes can be revisited
     push!(visited, newnode)
     if newnode == "end"
-        println(join(visited, ','))
+        # println(join(visited, ','))
         return true
     end
 
-    nextnodes = [node for node in values(nodes[newnode]) if !((node in visited) && (node in smallnodes))]
+    nextnodes = String[]
+    for node in values(nodes[newnode])
+        # can visit any ONE smallnode up to maxvisits times; but can also visit all other small nodes up to once
+        smalls = [n for n in [visited; node] if n in smallnodes] # after adding node to visited, vector of smallnodes
+        countsmalls = [count(==(i), smalls) for i in unique(smalls)] # number of each unique smallnode
+        nummaxvisits = count(>=(maxvisits), countsmalls) # 
+        
+        
+        if !(node in smallnodes) || ((nummaxvisits < 2) && (maximum(countsmalls) <= maxvisits))
+            push!(nextnodes, node)
+        end
+    end
     if length(nextnodes) == 0
         return false
     end
+    # println(nextnodes)
     total = 0
     for nextnode in nextnodes
-        total += pathsearch(nextnode, copy(visited))
+        total += pathsearch(nextnode, copy(visited), maxvisits)
     end
     return total
 end
 
 
-# file = "data/day12_test.txt" # part 1 = 10; part 2 = 
-# file = "data/day12_test2.txt" # part 1 = 19; part 2 = 
-# file = "data/day12_test3.txt" # part 1 = 226; part 2 = 
-file = "data/day12.txt" # part 1 = 5157; part 2 = 
+# file = "data/day12_test.txt" # part 1 = 10; part 2 = 36
+# file = "data/day12_test2.txt" # part 1 = 19; part 2 = 103
+# file = "data/day12_test3.txt" # part 1 = 226; part 2 = 3509
+file = "data/day12.txt" # part 1 = 5157; part 2 = 144309; takes a few seconds to run...
 
-pathsearch("start", String[])
+(nodes, smallnodes) = readnodes(file)
+pathsearch("start", String[], 2)
 
-# results = day11(file)
+# results = day12(file)
 # println("Part 1: $(results[1])")
 # println("Part 2: $(results[2])")
