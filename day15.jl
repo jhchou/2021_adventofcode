48f956ecb9bea9cfbcdd5134f891c9ae7a23ee05
Joseph Chou <joe.nicu@gmail.com>
2021-12-15 16:45:22 -0500

    Day 15, part 1 + 2 solution, horrific, takes 15 minutes to run

--- day15.jl
+++ day15.jl
@@ -1,37 +1,41 @@
-struct Digraph{T <: Real,U}
-    edges::Dict{Tuple{U,U},T}
-    verts::Set{U}
-end
- 
-function Digraph(edges::Vector{Tuple{U,U,T}}) where {T <: Real,U}
-    vnames = Set{U}(v for edge in edges for v in edge[1:2])
-    adjmat = Dict((edge[1], edge[2]) => edge[3] for edge in edges)
-    return Digraph(adjmat, vnames)
-end
- 
-vertices(g::Digraph) = g.verts
-edges(g::Digraph)    = g.edges
- 
-neighbours(g::Digraph, v) = Set((b, c) for ((a, b), c) in edges(g) if a == v)
- 
-function dijkstrapath(g::Digraph{T,U}, source::U, dest::U) where {T, U}
-    @assert source ∈ vertices(g) "$source is not a vertex in the graph"
- 
+function dijkstrapath(grid, source, dest, expand = false)
+    (maxx, maxy) = size(grid)
+    if expand
+        maxx *= 5
+        maxy *= 5
+    end
     # Easy case
     if source == dest return [source], 0 end
     # Initialize variables
-    dist = Dict(v => Inf for v in vertices(g))
-    prev = Dict(v => v   for v in vertices(g))
+    dist = Dict{Tuple{Int, Int}, Float64}() # vertex as key --> initialize at Inf distance
+    for x in 1:maxx, y in 1:maxy
+        dist[(x, y)] = Inf
+    end
     dist[source] = 0
-    Q = copy(vertices(g))
-    neigh = Dict(v => neighbours(g, v) for v in vertices(g))
+
+    prev = Dict{Tuple{Int, Int}, Tuple{Int, Int}}()
+    for x in 1:maxx, y in 1:maxy
+        prev[(x, y)] = (x, y)
+    end
+
+    Q = Set(keys(prev))
  
     # Main loop
     while !isempty(Q)
         u = reduce((x, y) -> dist[x] < dist[y] ? x : y, Q)
         pop!(Q, u)
         if dist[u] == Inf || u == dest break end
-        for (v, cost) in neigh[u]
+
+        neigh = (Tuple{Tuple{Int64, Int64}, Float64})[]
+        (x0, y0) = u
+        for (Δx, Δy) in [(-1, 0), (1, 0), (0, -1), (0, 1)] # left, right, up, down
+            (x, y) = (x0 + Δx, y0 + Δy)
+            if x >= 1 && x <= maxx && y >=1 && y <= maxy
+                push!(neigh, ((x, y), gridplus(grid, x, y)))
+            end
+        end
+
+        for (v, cost) in neigh
             alt = dist[u] + cost
             if alt < dist[v]
                 dist[v] = alt
@@ -41,7 +45,7 @@ function dijkstrapath(g::Digraph{T,U}, source::U, dest::U) where {T, U}
     end
  
     # Return path
-    rst, cost = U[], dist[dest]
+    rst, cost = Tuple{Int, Int}[], dist[dest]
     if prev[dest] == dest
         return rst, cost
     else
@@ -54,8 +58,9 @@ function dijkstrapath(g::Digraph{T,U}, source::U, dest::U) where {T, U}
     end
 end
 
-file = "data/day15_test.txt" # part 1 = 40; part 2 = x # 10 x 10
-# file = "data/day15.txt" # part 1 = 537; part 2 = # 100 x 100
+
+# file = "data/day15_test.txt" # part 1 = 40; part 2 = 315 # 10 x 10
+file = "data/day15.txt" # part 1 = 537; part 2 = 2881 # 100 x 100; takes a LONG time to run
 
 
 # Convert file of grid of digits into matrix of Int: https://www.assertnotmagic.com/2019/05/17/julia-read-grid/
@@ -63,20 +68,20 @@ lines = readlines(file) .|> collect .|> x -> parse.(Int, x)
 grid = permutedims(hcat(lines...)) # without ..., get 5x1 matrix of Vector, instead of 10x5 matrix of Int
 (maxx, maxy) = size(grid)
 
-graph = Vector{Tuple{String, String, Int}}()
-
-for x0 in 1:maxx, y0 in 1:maxy # traverse grid as DESTINATIONS
-    for (Δx, Δy) in [(-1, 0), (1, 0), (0, -1), (0, 1)] # left, right, up, down
-        (x, y) = (x0 + Δx, y0 + Δy)
-        if checkbounds(Bool, grid, x, y)
-            push!(graph, ("$x,$y", "$x0,$y0", grid[x0, y0]))
-        end
-    end
+function gridplus(grid, x, y) # return value at 5x expanded grid at [x,y]
+    (maxx, maxy) = size(grid)
+    x > maxx*5 || y > maxy*5 && return nothing # out of bounds
+    incx = (x-1) ÷ maxx # 0, 1, 2, 3, 4
+    incy = (y-1) ÷ maxy # 0, 1, 2, 3, 4
+    x0 = x - incx * maxx
+    y0 = y - incy * maxy
+    val = grid[x0, y0] + incx + incy
+    val = val <= 9 ? val : val - 9
+    return val
 end
 
-g = Digraph(graph)
-src, dst = "1,1", "100,100"
-path, cost = dijkstrapath(g, src, dst)
+
+path, cost = dijkstrapath(grid, (1,1), (500,500), true)
 println("Shortest path from $src to $dst: ", isempty(path) ? "no possible path" : join(path, " → "), " (cost $cost)")
 
 
