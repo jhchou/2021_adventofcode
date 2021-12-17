11dcc75fca70ca7a178fc2129012d47f47ea86df
Joseph Chou <joe.nicu@gmail.com>
2021-12-17 15:52:30 -0500

    Day 17, part 1 + 2, refactored into function

--- day17.jl
+++ day17.jl
@@ -1,5 +1,7 @@
-
 fireprobe = function(vx, vy, goalxmin, goalxmax, goalymin, goalymax)
+    # given starting information, return:
+    # - Bool of whether target reached
+    # - max y height reached
     ymax = -Inf
     (xpos,ypos) = (0,0)
     reachtarget = false
@@ -24,51 +26,50 @@ fireprobe = function(vx, vy, goalxmin, goalxmax, goalymin, goalymax)
     end
 end
 
-# (vx, vy) = (6, 3)
-# (vx, vy) = (9, 0)
-# (vx, vy) = (17,-4)
-# (vx, vy) = (6, 9)
-# fireprobe(vx, vy, goalxmin, goalxmax, goalymin, goalymax)
 
+day17 = function(input)
+    m = match(r"x=(.+)\.\.(.+), y=(.+)\.\.(.+)", input)
+    (goalxmin, goalxmax, goalymin, goalymax) = m.captures .|> x -> parse(Int, x)
 
-
-
-# input = "target area: x=20..30, y=-10..-5"
-input = "target area: x=111..161, y=-154..-101"
-m = match(r"x=(.+)\.\.(.+), y=(.+)\.\.(.+)", input)
-(goalxmin, goalxmax, goalymin, goalymax) = m.captures .|> x -> parse(Int, x)
-
-
-# Determine maximum ranges of x velocity: vxmin to vxmax
-vxmin = 1
-while true # keep incrementing vxmin until able to reach goalxmin before stopping
-    x = 0
-    for vxstep in range(start = vxmin, stop = 0, step = -1)
-        x += vxstep
+    # Determine maximum ranges of x velocity: vxmin to vxmax
+    vxmin = 1
+    while true # keep incrementing vxmin until able to reach goalxmin before stopping
+        x = 0
+        for vxstep in range(start = vxmin, stop = 0, step = -1)
+            x += vxstep
+        end
+        if x >= goalxmin break end
+        vxmin += 1
     end
-    if x >= goalxmin break end
-    vxmin += 1
-end
-vxmax = goalxmax + 1
+    vxmax = goalxmax + 1
+    
+    # Determine maximum ranges of y velocity: vymin to vymax
+    vymin = goalymin - 1 # any more negative and it will undershoot immediately
+    # not sure how to bound vymax
+    vymax = 1000 # just a guess
 
-vymin = goalymin - 1 # any more negative and it will undershoot immediately
-# not sure how to bound vymax
-vymax = 1000
 
-ymax = -Inf
-success = Set{Tuple{Int, Int}}()
-for vx0 in vxmin:vxmax
-    for vy0 in vymin:vymax
-        (reachtarget, y) = fireprobe(vx0, vy0, goalxmin, goalxmax, goalymin, goalymax)
-        if reachtarget
-            push!(success, (vx0, vy0))
-            if y > ymax
-                ymax = y
-                println("$vx0 $vy0 $ymax")
+    # Loop through ranges of x and y and keep ymax over all attempts
+    ymax = -Inf
+    success = Set{Tuple{Int, Int}}()
+    for vx0 in vxmin:vxmax
+        for vy0 in vymin:vymax
+            (reachtarget, y) = fireprobe(vx0, vy0, goalxmin, goalxmax, goalymin, goalymax)
+            if reachtarget
+                push!(success, (vx0, vy0))
+                if y > ymax
+                    ymax = y
+                    # println("$vx0 $vy0 $ymax")
+                end
             end
         end
     end
+    return (ymax, length(success))    
 end
 
-println(ymax) # 11781
-println(length(success))
+# input = "target area: x=20..30, y=-10..-5" # part 1 = 45, part 2 = 112
+input = "target area: x=111..161, y=-154..-101" # part 1 = 11781, part 2 = 4531
+
+result = day17(input)
+println("Part 1: $(result[1])")
+println("Part 2: $(result[2])")
