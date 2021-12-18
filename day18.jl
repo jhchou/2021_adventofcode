eb87002255a4e54be6c253484c16b3fcfc31f618
Joseph Chou <joe.nicu@gmail.com>
2021-12-18 14:54:25 -0500

    Day 16, explode function

--- day18.jl
+++ day18.jl
@@ -12,20 +12,33 @@ explode = function(num)
         if c == '['
             nest_level += 1
             if nest_level > 4
+                m = match(r"^\[(\d+),(\d+)\]", num[idx:end]) # capture the first 4th nested pair
+                num1 = parse(Int, m.captures[1])
+                num2 = parse(Int, m.captures[2])
+                chars = length(m.match) # length of the match, including brackets + comma
+
                 # https://stackoverflow.com/questions/8742642/what-regex-string-will-find-the-last-rightmost-group-of-digits-in-a-string
-                m = match(r"^(.*?)(\d+)(\D*)$", num[1:idx-1])
-                if m  == nothing
+                pre = num[1:idx-1]
+                m = match(r"^(.*?)(\d+)(\D*)$", pre) # .*? is NON-greedy match
+                if m  === nothing
                     prenum = -1
+                    prenumidx = -1
                 else
-                    prenum = parse(Int, m.captures[2]) # need indices, m.offsets[2] is location of first digit
+                    prenum = m.captures[2] # keep as string, will need length
+                    prenumidx = m.offsets[2]
+                    pre = pre[1:m.offsets[2] - 1] * string(parse(Int, prenum) + num1) * pre[m.offsets[2]+length(prenum):end]
                 end
-                m = match(r"^(\D*)(\d+)(.*?)$", num[idx+5:end])
-                if m  == nothing
+                post = num[idx+chars:end]
+                m = match(r"^(\D*)(\d+)(.*?)$", post)
+                if m  === nothing
                     postnum = -1
+                    postnumidx = -1
                 else
-                    postnum = parse(Int, m.captures[2]) # need indices
+                    postnum = m.captures[2]
+                    postnumidx = m.offsets[2]
+                    post = post[1:postnumidx-1] * string(parse(Int, postnum) + num2) * post[postnumidx+length(postnum):end]
                 end
-                return(idx, idx+4, num[idx:idx+4], parse(Int, num[idx+1]), parse(Int, num[idx+3]), prenum, postnum)
+                return(pre*'0'*post)
             end
         elseif c == ']'
             nest_level -= 1
@@ -34,9 +47,9 @@ explode = function(num)
 end
 
 # Explode
-explode("[[[[[9,8],1],2],3],4]") # becomes [[[[0,9],2],3],4]
-explode("[7,[6,[5,[4,[3,2]]]]]") # becomes [7,[6,[5,[7,0]]]]
-explode("[[6,[5,[4,[3,2]]]],1]") # becomes [[6,[5,[7,0]]],3]
-explode("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]") # becomes [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]
-explode("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]") # becomes [[3,[2,[8,0]]],[9,[5,[7,0]]]]
+explode("[[[[[9,8],1],2],3],4]") == "[[[[0,9],2],3],4]"
+explode("[7,[6,[5,[4,[3,2]]]]]") == "[7,[6,[5,[7,0]]]]"
+explode("[[6,[5,[4,[3,2]]]],1]") == "[[6,[5,[7,0]]],3]"
+explode("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]") == "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+explode("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]") == "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
 
