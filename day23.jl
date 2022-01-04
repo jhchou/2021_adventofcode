7fb65acb3dc0d7cfa60c6ed62c83d268ff1f39bb
Joseph Chou <joe.nicu@gmail.com>
2022-01-04 08:58:01 -0500

    Day 23, part 2, continued refactoring

--- day23.jl
+++ day23.jl
@@ -1,15 +1,42 @@
+using AStarSearch # https://github.com/PaoloSarti/AStarSearch.jl
+
+part2 = false # inserts 32103102 at rows 3 and 4
+
 start = "9999999999912130320" # test, part 1 cost = 12521; part 2 cost = 44169
-# start = "9999999999930223011" # input, cost = 19046
+# start = "9999999999930223011" # input, cost = 19046; part 2 cost = ?
 # start = "9999999999932012301" # jean
 
-part2 = true # inserts 32103102 at rows 3 and 4
+
+# Rules
+# - Amber 1 energy per step; Bronze 10; Copper 100; Desert 1000
+# - will never stop on the space immediately outside any room (positions 3,5,7,9 in row 1)
+# - will never move into a room unless destination AND no amphipods there not their own destination
+# - once stops in hallway, will stay until it can move into its room
+#
+# Input
+#   #############
+#   #...........#    --> hallway 11 columns
+#   ###D#A#C#C###    --> positions 3, 5, 7, 9 -- off limits unless room valid to enter
+#     #D#A#B#B#
+#     #########
+#
+# String encoding: 11 characters of hallways followed by sets of 4 characters for rooms
+# - Empty spaces '9'
+# - Amphipods: A '0', B '1', C '2', D '3'
+#   - energy = 10^type
+#   - goal column for room = 2*type + 3
+#
+# Costs
+# - ?for heuristic -- account for only movement in first row?
+# - ?start room to hallway above start room: depends on input?
+# - hallway above destination to goal room: 3 * (1 + 10 + 100 + 1000) = 3333
+
+
 if part2
     start = start[1:15] * "32103102" * start[16:19]
 end
-
 roomrows = (length(start) - 11) ÷ 4
-
-
+goalstate = "99999999999" * "0123"^roomrows # hallway 11x 9's, followed by roomrows repetitions of "0123"
 
 
 # Build conversion dictionaries between idx and (row, col)
@@ -24,76 +51,24 @@ for idx in 1:(11 + 4*roomrows)
     end
     idx2rc[idx] = (row, col)
     rc2idx[row, col] = idx
-    println("$idx $row $col")
+    # println("$idx $row $col")
 end
 
 
+"Take state string and display graphic"
 function printstate(state; showstring=false)
+    roomrows = (length(state) - 11) ÷ 4
     x = replace(state, "9" => ".", "0" => "A", "1" => "B", "2" => "C", "3" => "D")
-    if showstring
-        println(state)
-    end
+    if showstring println(state) end
     println("#############")
     println("#$(x[1:11])#")
     println("###$(x[12])#$(x[13])#$(x[14])#$(x[15])###")
-    println("  #$(x[16])#$(x[17])#$(x[18])#$(x[19])#")
+    for i in 2:roomrows
+        println("  #$(x[i*4 + 8])#$(x[i*4 + 9])#$(x[i*4 + 10])#$(x[i*4 + 11])#")
+    end
     println("  #########")
     println()
-    println()
 end
-
-
-using AStarSearch # https://github.com/PaoloSarti/AStarSearch.jl
-
-# - Amber 1 energy per step; Bronze 10; Copper 100; Desert 1000
-# - will never stop on the space immediately outside any room (positions 3,5,7,9 in row 1)
-# - will never move into a room unless destination AND no amphipods there not their own destination
-# - once stops in hallway, will stay until it can move into its room
-
-# Test: minimum 12521 energy
-#   #############
-#   #...........#
-#   ###B#C#B#D###
-#     #A#D#C#A#
-#     #########
-
-# Input
-#   #############
-#   #...........#    --> hallway 11 columns
-#   ###D#A#C#C###    --> positions 3, 5, 7, 9 -- off limits unless room valid to enter
-#     #D#A#B#B#      --> 3 rows total
-#     #########
-
-# String encoding: 19 characters
-# - (1-11: hallway chars)(12-15: 4 rooms in row 2)(16-19: 4 rooms in row 3)
-# - empty spaces '9'
-# - A '0', B '1', C '2', D '3'
-#   - energy = 10^type
-#   - goal column for room = 2*type + 3
-#
-# - room index position to column
-#   - 12-15: 2(idx-12) + 3
-#   - 16-19: 2(idx-16) + 3
-#
-# - for amphi in '0':'3' println(findall(==(amphi), collect("9999999999912130320"))) end
-#
-# Test:  "9999999999912130320"
-# Input: "9999999999930223011"
-#
-# Goal
-# - room column --> hallway 1,2,4,6,8,10,11 --> goal room column
-# - ?for heuristic -- account for only movement in first row?
-#
-# Valid movements
-# - for each of the 8 amphipods
-#   - check for blockages
-#   - move from room to hallway 1,2,4,6,8,10,11
-#   - move from hallway to goal room
-#
-# Constant energy costs
-# - ?start room to hallway above start room: depends on input?
-# - hallway above destination to goal room: 3 * (1 + 10 + 100 + 1000) = 3333
-
 
 
 "Heuristic function"
@@ -111,6 +86,7 @@ function h(state, goal)
     end
     return cost
 end
+
 
 "Cost between two states"
 function statecost(state1, state2)
@@ -130,6 +106,7 @@ function statecost(state1, state2)
     cost = 10^type * (abs(row2 - row1) + abs(col2 - col1))
     return cost
 end
+
 
 "Swap characters at 2 indices"
 function swapidx(state, idx1, idx2)
@@ -182,25 +159,11 @@ function findneighbors(state)
 end
 
 
-start = "9999999999912130320" # test, cost = 12521
-# start = "999999999991213321031020320" # test, part 2, cost = 44169; adds 32103102 at rows 3 and 4
-
-# start = "9999999999930223011" # input, cost = 19046
-# start = "999999999993022321031023011" # input, part 2, cost = 
-
-# start = "9999999999932012301" # jean
-
-goalstate = "9999999999901230123"
-# goalstate = "999999999990123012301230123" # for part 2
-
 
 result = astar(findneighbors, start, goalstate, heuristic = h, cost = statecost)
 
 result.status
-result.cost
 result.closedsetsize
 result.opensetsize
-
-printstate.(result.path, showstring = false)
-
-
+printstate.(result.path, showstring = false);
+result.cost
